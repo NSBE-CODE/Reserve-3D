273d8e0107aea5470495208cbb340323054ac0d1
cosimawest <44038882+cosimawest@users.noreply.github.com>
2020-06-03 00:39:06 -0400

    Landing Page Form

--- src/components/all/Footer.js
+++ src/components/all/Footer.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import '../../styles/all/footer.css';
 
+import UF_Signature from '../../assets/landing/UF_Signature-white-800.png';
+
+
 
 const Footer = (props) => {
 
     return (
         <div className = "footer" >
-            <h2> Footer </h2>
+            <img src={UF_Signature} style={{height:"34px", marginTop:"17px"}}/>
         </div>
     )
 };
