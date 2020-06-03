fd23848d440f1c84481c6ba21ba13eb54c764750
cosimawest <44038882+cosimawest@users.noreply.github.com>
2020-06-03 12:49:15 -0400

    ...

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -7,25 +7,7 @@ const Landing = (props) => {
 
     return (
         <div className="landing">
-            <div className="reservation-form-container">
-                <form>
-                    <h3>Choose a Date</h3>
-                    <input id="field" type="date" placeholder="Date MM/DD/YYYY"></input>
-                    <input id="field" type="number" placeholder="Est. Required Time HH:MM"></input>
-                    <select id="field" placeholder="jdisoafh">
-                        <option>Select Printer Size</option>
-                        <option value="medium">Medium</option>
-                        <option value="large">Large</option>
-                    </select>
-                    <input id="field" type="text" placeholder="Comments..."></input>
-
-                    <button type="submit">Search</button>
-                </form>
-            </div>
-
-            <div className="time-selection-container">
-                <h1>THIS IS THE TIME SELECTION COMPONENT</h1>
-            </div>
+            <h1>THIS IS THE LANDING PAGE.</h1>
         </div>
     )
 };
