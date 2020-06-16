a346cff9bce4005143ac5ba013f7c69f73a0dd04
Z. George <georgez1@ufl.edu>
2020-06-16 17:59:01 -0400

    Working on CRUD Functions

--- src/server/models/Gallery.js
+++ src/server/models/Gallery.js
@@ -1,1 +1,30 @@
-//Model
+/*
+Gallery.js (note: are we storing the info of the cam footages?)
+    image
+    description
+CRUD
+    PUT
+    POST
+    DELETE
+    GET (single entry)
+    GET (all entries)
+*/
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var GallerySchema = new Schema({
+    image: { data: Buffer, contentType: String },
+    description: String,
+});
+
+
+//PUT
+//POST
+//DELETE
+//GET (single entry)
+//GET (all entries)
+
+
+
+
+module.exports = mongoose.model('Gallery', GallerySchema);
--- src/server/models/LabHours.js
+++ src/server/models/LabHours.js
@@ -1,1 +1,93 @@
-//Model
+/*
+LabHours.js
+    (note: days will be deleted after passing) GET through date
+    Date (YYYYMMDD) 
+    Open (bool)
+    Hours[]
+        Hour (double)
+        Available (bool)
+        LargePrintersAvailable (int)
+        MediumPrintersAvailable (int) 
+CRUD
+    PUT
+    POST
+    DELETE
+    GET (single entry from date)
+    GET (7 entries. Two before the date, the date, and two after the date)
+    no get all function needed
+*/
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var LabHoursSchema = new Schema({
+    Date: Date,
+    Open: Boolean,
+    Hours: {
+        Hour: Number,
+        Available: Boolean,
+        LargePrintersAvailable: Number,
+        MediumPrintersAvailable: Number,
+    },
+});
+
+//CRUD Functions
+//Get
+LabHoursSchema.statics = {
+    getByDate: function(req,res) {
+        this.findOne({
+            Date: req.body.Date
+        })
+        .exec()
+        .then((labHour)=>{
+            res.json(labHour)
+        })
+        .catch((err)=>{
+            console.log('Error occured: ' + err);  
+        });
+    },
+    //Get Dates 2 days before and after the selected date
+    get5ByDate: function(req,res) {
+        var date = new Date(req.body.Date);
+        var end = new Date(date.setDate(date.getDate() + 2));
+        var begin = new Date(date.setDate(date.getDate() - 4));
+        this.find({
+            Date: {$gte: begin, $lte: end}
+        })
+        .exec()
+        .then((labHour)=>{
+            res.json(labHour);
+        })
+        .catch((err)=>{
+            console.log('Error occured: ' + err);  
+        });
+    },
+    //POST
+    post: function(req,res) {
+        var labHour = req.body;
+        this.create(labHour)
+        .then((labHour)=>{
+            res.json(labHour);
+        })
+        .catch((err)=>{
+            console.log('Error occured: ' + err);  
+        });
+    },
+    //DELETE
+    post: function(req,res) {
+        var labHour = req.body;
+        this.create(labHour)
+        .then((labHour)=>{
+            res.json(labHour);
+        })
+        .catch((err)=>{
+            console.log('Error occured: ' + err);  
+        });
+    },
+};
+
+
+
+
+var LabHours = mongoose.model('LabHours', LabHoursSchema);
+module.exports = LabHours;
