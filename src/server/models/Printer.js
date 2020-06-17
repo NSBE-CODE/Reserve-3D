03f70d9bb4f0de957b3faa9c822da0a93fa83e14
Chance C. Onyiorah <51178636+chancethescholar@users.noreply.github.com>
2020-06-17 02:48:21 -0400

    Update Printer.js
    initial commit of printer model

---
+++ src/server/models/Printer.js
@@ -0,0 +1,97 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var PrinterSchema = new Schema({
+  Name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+
+  Status: {
+    type: String,
+    required: true,
+  },
+
+  LastUsed: {
+    type: Date,
+  },
+
+  Size: {
+    type: Number,
+  },
+
+  Availabilty: {
+    type: String,
+    required: true,
+  },
+});
+
+//CRUD
+PrinterSchema.statics = {
+  // POST (get from _id)
+  post: function(req, res) {
+    var printer = req.body;
+    this.create(printer)
+    .then((printer) => {
+      res.json(printer);
+    })
+    .catch((err) => {
+      console.log('Error: ' + err);
+    });
+  },
+
+  //PUT
+  put: function(req, res) {
+    this.findOneAndUpdate({
+      Name: req.body.Name
+    },
+  )
+  .exec().then((printer) =>{
+    res.json(printer);
+  })
+  .catch((err) => {
+    console.log('Error: ' + err);
+  });
+},
+
+  // DELETE
+  delete: function(req, res) {
+    this.deleteOne({
+      Name: req.body.Name
+    })
+    .then((printer) => {
+      res.json(printer);
+    })
+    .catch((err) => {
+      console.log('Error: ' + err);
+    });
+  },
+
+  //GET (single entry)
+  getByName: function(req, res) {
+    this.findOne({
+      Name: req.body.Name
+    })
+    .exec().then((printer) => {
+      res.json(printer)
+    })
+    .catch((err) => {
+      console.log('Error: ' + err);
+    });
+  },
+
+  // GET (all entries)
+  get: function(req, res) {
+    this.find({})
+    .then((printer) => {
+      res.json(printer);
+    })
+    .catch((err) => {
+      console.log('Error: ' + err);
+    });
+  },
+};
+
+var Printer = mongoose.model('Printer', PrinterSchema);
+module.exports = Printer;
