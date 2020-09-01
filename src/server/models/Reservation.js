79134995131de4bba28040c81af38895387b52be
Charles Charlestin <ccharlestin@gmail.com>
2020-09-01 18:45:36 -0400

    Reservation CRUD Completed

--- src/server/models/Reservation.js
+++ src/server/models/Reservation.js
@@ -33,7 +33,82 @@ var ReservationSchema = new Schema({
   }
 });
 
-//Check with everyone to see if CRUD is necessary
+ReservationSchema.statics = {
+  // POST
+  post: function(req,res) {
+    var reservation = req.body;
+    this.create()
+    .then((reservation)=>{
+        res.json(reservation);
+    })
+    .catch((err)=>{
+        console.log('Error occured: ' + err);  
+    });
+},
+
+  //PUT
+  put: function(req, res) {
+    this.findById({
+      _id: req.params.id
+    },
+  )
+  .exec().then((reservation) =>{
+    res.json(reservation);
+  })
+  .catch((err) => {
+    console.log('Error: ' + err);
+  });
+},
+
+  // DELETE
+  delete: function(req, res) {
+    this.deleteOne({
+      Name: req.body.Name
+    })
+    .then((reservation) => {
+      res.json(reservation);
+    })
+    .catch((err) => {
+      console.log('Error: ' + err);
+    });
+  },
+
+  //GET  (single entry by reserver_id)
+  getByName: function(req, res) {
+    this.findOne({
+      Name: req.body.Reserver_id
+    })
+    .exec().then((reservation) => {
+      res.json(reservation)
+    })
+    .catch((err) => {
+      console.log('Error: ' + err);
+    });
+  },
+
+  // GET (multiple entries by date)
+  get: function(req, res) {
+    this.find({Date: req.body.Date})
+    .then((reservation) => {
+      res.json(reservation);
+    })
+    .catch((err) => {
+      console.log('Error: ' + err);
+    });
+  },
+
+  // GET (all entries)
+  get: function(req, res) {
+    this.find({})
+    .then((reservation) => {
+      res.json(reservation);
+    })
+    .catch((err) => {
+      console.log('Error: ' + err);
+    });
+  },
+};
+
 
 var Reservation = mongoose.model('Reservation', ReservationSchema);
 module.exports = Reservation;
